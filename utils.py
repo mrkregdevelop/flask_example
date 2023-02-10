d836a7ac52a08a8d17a9a57ef0538cdd2acc2714
Taras <taras.sheketa@gmail.com>
2023-02-10 19:43:12 +0200

    commit 2

---
+++ utils.py
@@ -0,0 +1,21 @@
+import string
+import random
+
+
+def generate_password(length: int) -> str:
+    chars = string.ascii_letters + string.digits
+    str_password = ''
+
+    for _ in range(length):
+        str_password += random.choice(chars)
+
+    return str_password
+
+
+def execute_sql(sql: str) -> None:
+    import sqlite3
+    con = sqlite3.connect("tutorial.db")
+    cur = con.cursor()
+    cur.execute(sql)
+    con.commit()
+    con.close()
